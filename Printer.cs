52ede799ee819b90afade6197627cfec2aa24d1e
dev3184 <devbhalani3184@gmail.com>
2025-09-25 14:50:27 +0530

    add S of SOLID principle

---
+++ Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Printer
+    {
+        public void PrintArea(Circle circle)
+        {
+            Console.WriteLine($"Area of the shape: {circle}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine($"Current time is: {DateTime.Now}");
-Console.WriteLine($"Current time is: {DateTime.Now}");
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            CalculateArea(circle);
+            new Printer().PrintArea(circle);
+
+        }
+        private static void CalculateArea(Circle circle)
+        {
+            double area = circle.Area();
+            Console.WriteLine($"Area of the circle: {area}");
+        }
+    }
+}
