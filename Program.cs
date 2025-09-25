a30512551a15ccd122e812e82a9f0493779604d8
dev3184 <devbhalani3184@gmail.com>
2025-09-25 14:50:27 +0530

    added time feature

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine($"Current time is: {DateTime.Now}");
+Console.WriteLine($"Current time is: {DateTime.Now}");
